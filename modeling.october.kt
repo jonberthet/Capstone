ccb1d926c9eeaa55489517a91c0ef90928e2ff5a
kjtong <kjtong@uchicago.edu>
2015-10-20 19:04:33 -0500

    Update modeling.october.kt

--- modeling.october.kt
+++ modeling.october.kt
@@ -2,7 +2,9 @@ library(caret)
 library(car)
 library(MASS)
 library(gvlma)
+library(party)
 load("/project/msca/capstone3/patient_matrix2revise.RData")
+load("/project/msca/kjtong/capstone.october.RData")
 
 #Test and Train for Multicollinearity on numerical variables
 set.seed(10)
@@ -30,7 +32,8 @@ traindf<-training_set[,-highCorr] #remove highly correlated predictors from trai
 testdf<-testing_set[,-highCorr] #remove highly correlated predictors from testing set
 ncol(traindf)
 ncol(testdf)
-#Do lasso or ridge
+
+
 #Build benchmark linear regression from reduced data and look at residuals to determine if further transformations are needed
 fulllm<-lm(y2_charges~., data=traindf)
 summary(fulllm)
@@ -38,7 +41,7 @@ fulllm.MSE <- mean((fulllm$fitted.values - traindf$y2_charges)^2)
 fulllm.MSE
 
 #Look at Residuals and outliers
-outlierTest(fulllm)
+outlier<-outlierTest(fulllm)
 #qqPlot(fulllm)
 #leveragePlot(fulllm)
 #av.Plots(fulllm)
@@ -46,7 +49,7 @@ sresid<-studres(fulllm)
 hist(sresid, freq=FALSE)
 curve(dnorm, add=TRUE)
 #non-constant error variance test
-ncvTest(fulllm)
+ncvtest<-ncvTest(fulllm)
 #Non-constant Variance Score Test 
 #Variance formula: ~ fitted.values 
 #Chisquare = 141101.3    Df = 1     p = 0 #data is significantly heteroscedastic
@@ -62,9 +65,9 @@ hist(resid(fulllm))
 
 #Evaluate nonlinearity
 #crPlots(fulllm)
-ceresPlots(fulllm)
+#ceresPlots(fulllm)
 #Test for Autocorrelated Errors
-durbinWatsonTest(fulllm)
+durbinwatson<-durbinWatsonTest(fulllm)
 #Global test for model assumptions
 gvmodel<-gvlma(fulllm)
 summary(gvmodel)
@@ -89,7 +92,7 @@ xtest<-predict(xTrans, testdf)
 
 #Machine learning models
 #regression tree
-dftree<-patient_matrix
+dftree<-patient_matrix[1:500,]
 dftree$patient_id<-NULL
 set.seed(10)
 
@@ -101,19 +104,53 @@ x1_test <- model.matrix(y2_charges~.,testing_set_tree)
 y1_train <- training_set_tree$y2_charges
 y1_test <- testing_set_tree$y2_charges
 
-bootControl<-trainControl(number=200)
+formula<-y2_charges~.
+
+fit.ctree.party<-ctree(formula, data=training_set_tree, controls=ctree_control(mincriterion=0.95,savesplitstats=FALSE))
+
+
+bootControl<-trainControl(method="cv", number=6, summaryFunction=defaultSummary)
 set.seed(10)
-treefit<-train(x1_train, y1_train,method="ctree", tuneLength=5, trControl=bootControl, scaled=FALSE)
+getModelInfo("ctree2", FALSE)[[1]]$grid
+modelLookup("ctree2")
+Grid<-expand.grid(maxdepth=seq())
+treefit<-train(x1_train, y1_train,method="ctree2", tuneGrid=expand.grid(mincriterion=0.95))
 treefit
 treefit$finalModel
 
 
 #boosted tree
-gbmGrid<-expand.grid(.interaction.depth=(1:5)*2, .n.trees=(1:10)*25, .shrinkage=.1)
+MCLogLoss <- function(data, lev = NULL, model = NULL)  {
+  
+  obs <- model.matrix(~data$obs - 1)
+  preds <- data[, 3:ncol(data)]
+  
+  err = 0
+  for(ob in 1:nrow(obs))
+  {
+    for(c in 1:ncol(preds))
+    {
+      p <- preds[ob, c]
+      p <- min(p, 1 - 10e-15)
+      p <- max(p, 10e-15)
+      err = err + obs[ob, c] * log(p)
+    }
+  }
+  
+  out <- err / nrow(obs) * -1
+  names(out) <- c("MCLogLoss")
+  out
+}
+getModelInfo("gbm", FALSE)[[1]]$grid
+modelLookup("gbm")
+control<-trainControl(method="repeatedCV", number=10, repeats=1, verboseIter=FALSE, returnResamp="all", classProbs=TRUE,)
+gbmGrid<-expand.grid(interaction.depth=seq(1,4), n.trees=(1:4)*50, shrinkage=.1, n.minobsinnode=10)
 set.seed(10)
-gbmFit<-train(training_set_tree,trainClass, method="gbm", trControl=bootControl, verbose=FALSE, bag.fraction=0.5, tuneGrid=gbmGrid)
+gbmFit<-train(x1_train, y1_train, method="gbm", trControl=control, tuneGrid=gbmGrid, metric="RMSE", verbose=FALSE)
 
 #Conduct visualizations
+
+#Show model comparisons
 
 #predict of new samples
 
@@ -130,4 +167,32 @@ testValues<-subset(predValues, dataType=="Test")
 head(testValues)
 table(testValues$model)
 
+
+
+
+
+
+
+#str(dftree)
+
+
+
+
+regression.tree<-tree(y2_charges~.,dftree,subset=traintree)
+summary(regression.tree)
+
+plot(regression.tree)
+text(regression.tree,pretty=0)
+cv.regressiontree=cv.tree(regression.tree)
+plot(cv.regressiontree$size,cv.regressiontree$dev,type="b")
+
+prune.regressiontree=prune.tree(regression.tree,best=7)
+plot(prune.regressiontree)
+text(prune.regressiontree,pretty=0)
+
+yhat=predict(regression.tree,newdata=testing_set_tree)
+regression.test=dftree[-traintree, "y2_charges"]
+plot(yhat,regression.test)
+abline(0,1)
+mean((yhat-regression.test)^2)
 
